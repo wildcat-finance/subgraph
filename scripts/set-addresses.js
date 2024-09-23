0b716ef1aa91cd54eac232280a3d699bb7e2b2a4
d1ll0n <dillon@wildcat.finance>
2024-09-23 13:53:55 -0400

    improve script

--- scripts/set-addresses.js
+++ scripts/set-addresses.js
@@ -1,20 +1,32 @@
-const fs = require('fs');
-const networks = require('../networks.json');
-const path = require('path');
-const network = process.argv[2];
-if (!networks[network]) {
-  throw Error(`No deployments for network: ${network}`);
+const fs = require("fs");
+const networks = require("../networks.json");
+const path = require("path");
+const networkId = process.argv[2];
+if (!networks[networkId]) {
+  throw Error(`No deployments for network: ${networkId}`);
 }
-const {
-  WildcatArchController,
-  WildcatSanctionsSentinel
-} = networks[network];
-const template = fs.readFileSync(path.join(__dirname, '../subgraph.template.yaml'), 'utf8');
-const subgraph = template
-  .replace(/{{WildcatArchControllerAddress}}/g, WildcatArchController.address)
-  .replace(/{{WildcatArchControllerStartBlock}}/g, WildcatArchController.startBlock)
-  .replace(/{{WildcatSanctionsSentinelAddress}}/g, WildcatSanctionsSentinel.address)
-  .replace(/{{WildcatSanctionsSentinelStartBlock}}/g, WildcatSanctionsSentinel.startBlock)
-  .replace(/{{NetworkName}}/g, network)
+const { name: network, contracts } = networks[networkId];
+let subgraph = fs
+  .readFileSync(path.join(__dirname, "../subgraph.template.yaml"), "utf8")
+  .replace(new RegExp(`{{NetworkName}}`, "g"), network);
+let uncrashable = fs
+  .readFileSync(
+    path.join(__dirname, "../uncrashable-config.template.yaml"),
+    "utf8"
+  )
+  .replace(new RegExp(`{{NetworkName}}`, "g"), network);
+for (const contract in contracts) {
+  const { address, startBlock } = contracts[contract];
+  subgraph = subgraph
+    .replace(new RegExp(`{{${contract}Address}}`, "g"), address)
+    .replace(new RegExp(`{{${contract}StartBlock}}`, "g"), startBlock);
+  uncrashable = uncrashable
+    .replace(new RegExp(`{{${contract}Address}}`, "g"), address)
+    .replace(new RegExp(`{{${contract}StartBlock}}`, "g"), startBlock);
+}
 
-fs.writeFileSync(path.join(__dirname, '../subgraph.yaml'), subgraph);
+fs.writeFileSync(path.join(__dirname, "../subgraph.yaml"), subgraph);
+fs.writeFileSync(
+  path.join(__dirname, "../uncrashable-config.yaml"),
+  uncrashable
+);
