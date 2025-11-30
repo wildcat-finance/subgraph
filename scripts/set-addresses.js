e353b4ab7801fd698b2c990dce150448d07d23f1
d1ll0n <dillon@wildcat.finance>
2025-11-30 14:25:47 -0800

    write correct version of network-specific abi files before deployment

--- scripts/set-addresses.js
+++ scripts/set-addresses.js
@@ -5,28 +5,74 @@ const networkId = process.argv[2];
 if (!networks[networkId]) {
   throw Error(`No deployments for network: ${networkId}`);
 }
-const { name: network, contracts } = networks[networkId];
-let subgraph = fs
-  .readFileSync(path.join(__dirname, "../subgraph.template.yaml"), "utf8")
-  .replace(new RegExp(`{{NetworkName}}`, "g"), network);
-let uncrashable = fs
-  .readFileSync(
-    path.join(__dirname, "../uncrashable-config.template.yaml"),
-    "utf8"
-  )
-  .replace(new RegExp(`{{NetworkName}}`, "g"), network);
-for (const contract in contracts) {
-  const { address, startBlock } = contracts[contract];
-  subgraph = subgraph
-    .replace(new RegExp(`{{${contract}Address}}`, "g"), address)
-    .replace(new RegExp(`{{${contract}StartBlock}}`, "g"), startBlock);
-  uncrashable = uncrashable
-    .replace(new RegExp(`{{${contract}Address}}`, "g"), address)
-    .replace(new RegExp(`{{${contract}StartBlock}}`, "g"), startBlock);
+let subgraphTemplateYamlName = 'subgraph.template.yaml';
+if (networkId.toLowerCase().includes("plasma")) {
+  subgraphTemplateYamlName = "plasma-subgraph.template.yaml";
+}
+function setNetworkAddresses() {
+  const { name: network, contracts } = networks[networkId];
+  let subgraph = fs
+    .readFileSync(path.join(__dirname, `../${subgraphTemplateYamlName}`), "utf8")
+    .replace(new RegExp(`{{NetworkName}}`, "g"), network);
+  let uncrashable = fs
+    .readFileSync(
+      path.join(__dirname, "../uncrashable-config.template.yaml"),
+      "utf8"
+    )
+    .replace(new RegExp(`{{NetworkName}}`, "g"), network);
+  for (const contract in contracts) {
+    const { address, startBlock } = contracts[contract];
+    subgraph = subgraph
+      .replace(new RegExp(`{{${contract}Address}}`, "g"), address)
+      .replace(new RegExp(`{{${contract}StartBlock}}`, "g"), startBlock);
+    uncrashable = uncrashable
+      .replace(new RegExp(`{{${contract}Address}}`, "g"), address)
+      .replace(new RegExp(`{{${contract}StartBlock}}`, "g"), startBlock);
+  }
+
+  fs.writeFileSync(path.join(__dirname, "../subgraph.yaml"), subgraph);
+  fs.writeFileSync(
+    path.join(__dirname, "../uncrashable-config.yaml"),
+    uncrashable
+  );
 }
 
-fs.writeFileSync(path.join(__dirname, "../subgraph.yaml"), subgraph);
-fs.writeFileSync(
-  path.join(__dirname, "../uncrashable-config.yaml"),
-  uncrashable
-);
+function replaceNetworkAbis() {
+  const networkAbisDir = path.join(
+    __dirname,
+    "../network-specific-abis",
+    networkId
+  );
+  const abisDir = path.join(__dirname, "../abis");
+  if (!fs.existsSync(networkAbisDir)) {
+    throw new Error(`Network ${networkId} not found in network-specific-abis`);
+  }
+  const abiFiles = fs.readdirSync(networkAbisDir);
+
+  for (const abiFile of abiFiles) {
+    const abi = fs.readFileSync(path.join(networkAbisDir, abiFile), "utf8");
+    const abiPath = path.join(abisDir, abiFile);
+    fs.writeFileSync(abiPath, abi);
+  }
+}
+
+function replaceRefsToForceBuyBacks() {
+  const hooksFactoryPath = path.join(__dirname, "../src/hooks-factory.ts");
+  let hooksFactory = fs.readFileSync(hooksFactoryPath, "utf8");
+  if (networkId !== "sepolia") {
+    hooksFactory = hooksFactory.replaceAll(
+      "allowForceBuyBacks = hookedMarket.allowForceBuyBacks;",
+      "allowForceBuyBacks = false;"
+    );
+  } else {
+    hooksFactory = hooksFactory.replaceAll(
+      "allowForceBuyBacks = false;",
+      "allowForceBuyBacks = hookedMarket.allowForceBuyBacks;"
+    );
+  }
+  fs.writeFileSync(hooksFactoryPath, hooksFactory);
+}
+
+setNetworkAddresses();
+replaceNetworkAbis();
+replaceRefsToForceBuyBacks();
